39e97af40065a49a18ca1febf2d85111199a5539
Sokol111 <igorsokol111@gmail.com>
2026-05-27 09:04:44 +0200

    u

--- gen/typescript/api.schemas.ts
+++ gen/typescript/api.schemas.ts
@@ -3,7 +3,7 @@
  * Do not edit manually.
  * Image Service API
  * API for managing images
- * OpenAPI spec version: 1.2.1
+ * OpenAPI spec version: 1.2.2
  */
 export type OwnerType = typeof OwnerType[keyof typeof OwnerType];
 
