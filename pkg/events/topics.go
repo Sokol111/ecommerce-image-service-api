6ba4e25b63300f7baea05d6ffb3d8f6e1e5d7b83
Sokol111 <igorsokol111@gmail.com>
2026-07-02 16:19:48 +0200

    u

---
+++ pkg/events/topics.go
@@ -0,0 +1,28 @@
+package events
+
+import (
+	eventsv1 "github.com/Sokol111/ecommerce-image-service-api/gen/events/image/v1"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
+)
+
+// Topic constants
+const (
+	TopicCatalogProductEvents = "catalog.product.events"
+)
+
+// topicMap maps proto message full names to their Kafka topics.
+var topicMap = map[protoreflect.FullName]string{
+	(&eventsv1.ProductImagePromotedEvent{}).ProtoReflect().Descriptor().FullName(): TopicCatalogProductEvents,
+}
+
+// TopicFor returns the Kafka topic for the given proto message.
+// Panics if the message type is not registered in topicMap.
+func TopicFor(msg proto.Message) string {
+	fullName := msg.ProtoReflect().Descriptor().FullName()
+	topic, ok := topicMap[fullName]
+	if !ok {
+		panic("events: no topic registered for " + string(fullName))
+	}
+	return topic
+}
